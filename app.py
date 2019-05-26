d0d741a46dcd6137f42f8f02e412dc855d31abff
pawel <48882583+pawo161@users.noreply.github.com>
2019-05-26 23:24:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import random
 app = Flask(__name__)
 
 
@@ -8,7 +8,11 @@ def strona():
     return render_template('strona.html')
 @app.route('/tekst.html')
 def tekst():
-    return render_template('tekst.html')
+    if request.method == "POST":
+        number = random.randint(1, 30)
+        temp = open("gpt2_generated/gpt2_gentext%s.txt" % number, 'r')
+        l = ' '.join(map(str, temp))
+    return render_template('tekst.html', output=l)
 
 if __name__ == '  strona  ':
     app.run(Debug=True)
